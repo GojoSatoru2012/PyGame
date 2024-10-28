a7c32462f9e477a57aeec39b20fc3eff4a67cc15
GojoSatoru2012 <iliaburchu@gmail.com>
2024-10-28 19:55:28 +0400

    matchinggameicons.py

--- matching game/matchinggameicons.py
+++ matching game/matchinggameicons.py
@@ -1,24 +1,31 @@
 import pygame
 import random
 pygame.init()
+run = True
 screen = pygame.display.set_mode((600, 600))
 screen.fill("Red")
 #loading images
 subwaysurfer = pygame.image.load("subwaysurfers.png")
-minecraft = pygame.image.load("sminecraft.webp")
+minecraft = pygame.image.load("minecraft.webp")
 cs2 = pygame.image.load("cs2.jpg")
-templerun = pygame.image.load("templerun.avif")
+templerun = pygame.image.load("templerun.jpeg")
 roblox = pygame.image.load("Roblox.webp")
 #original correct position 
-dictionarygame = {"subwaysurfer", (150, 50),"minecraft", (150, 150),"roblox", (150, 250), "templerun", (150, 350), "cs2", (150, 450),}
+dictionarygame = {"subwaysurfer": (150, 50),"minecraft": (150, 150), "roblox": (150, 250), "templerun": (150, 350), "cs2": (150, 450)}
 #shuffling image position 
 imagepos = list(dictionarygame)
 random.shuffle(imagepos)
-imagepos1 = {name: dictionarygame[imagepos[i]] for i, name in enumerate(dictionarygame)}
+imagepos1 = {name: imagepos[i] for i, name in enumerate(dictionarygame)}
 #display images
 screen.blit(subwaysurfer, imagepos1["subwaysurfer"])
 screen.blit(templerun, imagepos1["templerun"])
 screen.blit(cs2, imagepos1["cs2"])
 screen.blit(roblox, imagepos1["roblox"])
 screen.blit(minecraft, imagepos1["minecraft"])
-pygame.display.update
+pygame.display.update()
+run = True
+while run:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+    pygame.quit()
