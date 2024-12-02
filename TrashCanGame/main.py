be7d394da5ffb472607f3e017e808d354b1af1a5
GojoSatoru2012 <iliaburchu@gmail.com>
2024-12-02 19:37:39 +0400

    Update main.py

--- TrashCanGame/main.py
+++ TrashCanGame/main.py
@@ -3,6 +3,9 @@ import random
 clock = pygame.time.Clock()
 from pygame.locals import *
 running = True
+score = 0
+scorefont = pygame.font.SysFont("sans serif", 19)
+text=scorefont.render("Score ="+str(score), True, "yellow")
 def background(bg):
     bg1  = pygame.image.load(bg)
     bg2 = pygame.transform.scale(bg1, (screen_width, screen_height))
@@ -13,11 +16,21 @@ screen_width = 800
 screen_height = 600
 screen = pygame.display.set_mode((screen_width, screen_height))
 class Bin(pygame.sprite.Sprite):
-    def __init__(self):
+    def __init__(self, speed = 5):
         super().__init__()
         self.image = pygame.image.load("Trash.webp").convert_alpha()
         self.image = pygame.transform.scale(self.image, (60,60))
         self.rect = self.image.get_rect()
+        self.speed = speed
+    def update(self, key):
+        if key[pygame.K_w]:
+            self.rect.move_ip(0,- self.speed)
+        if key[pygame.K_a]:
+            self.rect.move_ip(- self.speed,0)
+        if key[pygame.K_d]:
+            self.rect.move_ip(+ self.speed,0)
+        if key[pygame.K_s]:
+            self.rect.move_ip(0,+ self.speed)
 class Plasticbag(pygame.sprite.Sprite):
     def __init__(self):
         super().__init__()
@@ -49,15 +62,33 @@ for i in range(60):
     all.add(obj1)
 can = Bin()
 all.add(can)
-
-while running:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-    background("background.jpg")
-    all.draw(screen)
-    pygame.display.update()
-pygame.quit() mage, (40,60))
-        self.rect = self.image.get_rect()
-
+def startup():
+    global running
+    while running:
+        clock.tick(60)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+        key1 = pygame.key.get_pressed()
+        if key1[pygame.K_w]:
+            can.rect.move_ip(0,- can.speed)
+        if key1[pygame.K_a]:
+            can.rect.move_ip(- can.speed,0)
+        if key1[pygame.K_d]:
+            can.rect.move_ip(+ can.speed,0)
+        if key1[pygame.K_s]:
+            can.rect.move_ip(0,+ can.speed)
+        gcollide = pygame.sprite.spritecollide(can, allrec, True)
+        bcollide = pygame.sprite.spritecollide(can, notrec, True)
+        for i in gcollide:
+            score += 1
+            text=scorefont.render("Score ="+str(score), True, "yellow")
+        for i in bcollide:
+            score -= 5
+            text=scorefont.render("Score ="+str(score), True, "yellow")
+        background("background.jpg")
+        screen.blit(text, (500, 100))
+        all.draw(screen)
+        pygame.display.update()
+startup()
+pygame.quit() 
